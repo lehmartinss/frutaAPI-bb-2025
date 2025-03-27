0dce2dec99957660df366b53bac2dde3ca22ffb8
LETICIA BEATRIZ MARTINS <24122309@CFP127ALUNO.LOCAL>
2025-03-27 12:15:59 -0300

    ok

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# frutaAPI-bb-2025
+# API DE FRUTAS
+
+![](./Captura%20de%20tela%202025-03-27%20111844.png)
+
+---
+
+## Sobre 
+Projeto do curso de Desenvolvimento de Sistema do SENAI Jandira com o objetivo de utilizar API pública do site (https://www.fruityvice.com/) com javascrip [SENAI Jandira]. 
+(https://sp.senai.br/unidade/jandira/)
+
+
+---
+
+## Tecnologias Utilizadas
+- HTML
+- CSS
+- JS
+
+---
+
+## Autor
+- [Letícia Beatriz](https://www.linkedin.com/in/let%C3%ADcia-beatriz-martins-8933ba308/)
--- app.js
+++ app.js
@@ -22,7 +22,7 @@ async function retornarNomes(name){
     li.addEventListener('click', async (params) => {
         ul.style.display = 'none'
         const infoFruta = await pesquisa(name)
-        console.log(infoFruta);
+    
         
         frutainfo(infoFruta, info)
         info.style.display = 'flex'
@@ -39,26 +39,67 @@ async function pesquisa(name){
     const fruta = frutas.find(fruta => fruta.name === name)
     return fruta
 }
-function frutainfo(fruta, div){
-    const h1 = document.createElement('h1')
+function frutainfo(fruta,div){
+    const card = document.getElementById('info')
+    const informacoes = document.getElementById('informacoes')
+    const nome = document.getElementById('nome')
+    const infoFrutas = document.getElementById('infoFrutas')
+
+
+    const h1Id = document.createElement('p')
+    const h1Family = document.createElement('p')
+    const h1Order = document.createElement('p')
+    const h1Genus = document.createElement('p')
+    const h1Nutritions = document.createElement('p')
+    const h1Calories = document.createElement('p')
+    const h1Fat = document.createElement('p')
+    const h1Sugar = document.createElement('p')
+    const h1Carbohydrates = document.createElement('p')
+    const h1Protein = document.createElement('p')
+
+    const h1Nome = document.createElement('p')
+
     const spanID = document.createElement('span')
     const spanFamily = document.createElement('span')
     const spanOrder = document.createElement('span')
     const spanGenus = document.createElement('span')
-    const spanNutritions = document.createElement('span')
-    h1.textContent = fruta.name
+    const spanCalories = document.createElement('span')
+    const spanFat = document.createElement('span')
+    const spanSugar = document.createElement('span')
+    const spanCarbohydrates  = document.createElement('span')
+    const spanProtein = document.createElement('span')
+
+
+    h1Id.textContent = "Id:"
+    h1Family.textContent = "Family"
+    h1Order.textContent = "Order"
+    h1Genus.textContent = "Genus"
+    h1Nutritions.textContent = "Nutritions"
+    h1Calories.textContent = "Calories"
+    h1Fat.textContent = "Fat"
+    h1Sugar.textContent = "Sugar"
+    h1Carbohydrates.textContent = "Carbohydrates"
+    h1Protein.textContent = "Protein"
+
+
+    h1Nome.textContent = fruta.name
     spanID.textContent = fruta.id
     spanFamily.textContent = fruta.family
     spanOrder.textContent = fruta.order
     spanGenus.textContent = fruta.genus
-    spanNutritions.textContent = fruta.nutritions
+    spanCalories.textContent = fruta.calories
+    spanFat.textContent = fruta.fat
+    spanSugar.textContent = fruta.sugar
+    spanCarbohydrates.textContent = fruta.Carbohydrates
+    spanProtein.textContent = fruta.protein
 
-    div.appendChild(h1)
+
+    div.appendChild(h1Nome)
     div.appendChild(spanID)
     div.appendChild(spanFamily)
     div.appendChild(spanOrder)
     div.appendChild(spanGenus)
-    div.appendChild(spanNutritions)
+    
 }
 
 
@@ -87,78 +128,3 @@ document.getElementById('pesquisar')
         }
     })
 
-
-
-
-
-// function dadosFrutas(dados){
-//     const ul  = document.getElementById('lista')
-//     const li = document.createElement('li')
-//     const span = document.createElement('span')
-//     span.textContent = dados
-    
-//     li.appendChild(span)
-//     ul.appendChild(li)
-// }
-
-// async function renderDosDados(){
-//     const dadosDaFrutas = await pesquisarFrutasALL()
-//     dadosDaFrutas.forEach(dadosFrutas)
-// }
-
-// function apagarLista (){
-//     const base = document.getElementById('lista-container')
-//     base.style.display = "none"
-// }
-
-// document.getElementById('pesquisar')
-//     .addEventListener('click', async (params) => {
-//         if(document.getElementById('frutas').value == usuarioPesquisa){
-//             apagarLista()
-//                 renderDosDados()
-//         }
-//     })
-
-
-
-
-
-
-
-// async function pesquisarFamilia(){
-//     const url = `https://api.codetabs.com/v1/proxy?quest=https://www.fruityvice.com/api/fruit/family/:family` 
-
-//    const response = await fetch(url)
-//    const data = await response.json()
-//    const familiaFrutas = []
-//    data.forEach(function(frutas){
-//     familiaFrutas.push(frutas.family)
-//    })
-
-//    // console.log(familiaFrutas)
-//    return familiaFrutas
-
-// }
-
-// function retornarfamilia(family){
-//     const ul = document.getElementById('dadosfamilia')
-//     const li = document.createElement('li')
-//     const span = document.createElement('span')
-//     span.textContent = family
-    
-//     li.appendChild(span)
-//     ul.appendChild(li)
-// }
-
-// async function renderFamilia(){
-//     const familiaFrutas = await pesquisarFamilia()
-//     familiaFrutas.forEach(retornarfamilia)
-// }
-
-// document.getElementById('pesquisar')
-//     .addEventListener('click', async (params) => {
-//         if(document.getElementById('frutas').value == usuarioPesquisa){
-//             apagar()
-//                 render()
-//         }
-//     })
